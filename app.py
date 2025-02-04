621ce642e4b633dcf4729a3df2308ec2a679339b
junnnnghee <qhrehlwl111@gmail.com>
2025-02-04 13:15:56 +0900

    f

--- app.py
+++ app.py
@@ -6,16 +6,28 @@ from pandas.api.types import is_integer_dtype, is_float_dtype, is_object_dtype
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
-import platform
 
-if platform.system() == 'Windows':
-    plt.rcParams['font.family'] = 'Malgun Gothic'
-elif platform.system() == 'Linux':
-    plt.rcParams['font.family'] = 'NanumGothic'
 
-plt.rcParams['axes.unicode_minus'] = False
+import os
+import matplotlib.font_manager as fm
+
+@st.cache_data
+def fontRegistered():
+    font_dirs = [os.getcwd() + '/custom_fonts']
+    font_files = fm.findSystemFonts(fontpaths=font_dirs)
+    for font_file in font_files:
+        fm.fontManager.addfont(font_file)
+    fm._load_fontmanager(try_read_cache=False)
+
+
+
 
 def main():
+
+    fontRegistered()
+    plt.rc('font', family='나눔손글씨 다행체')
+
+    
     st.title('K-Means Clustering App')
 
     # 1. csv file upload
